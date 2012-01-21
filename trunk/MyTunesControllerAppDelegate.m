fe1d05fcef47fbcfdd573a5c992084bac284bbbb
Toomas Vahter <toomas.vahter@skype.net>
2012-01-21 11:52:53 +0200

    Broke down app delegate by adding StatusBarController class.

--- trunk/MyTunesControllerAppDelegate.m
+++ trunk/MyTunesControllerAppDelegate.m
@@ -32,200 +32,138 @@
 #import "PreferencesController.h"
 #import "iTunesController.h"
 #import "StatusView.h"
-#import "ImageScaler.h"
-
-@interface MyTunesControllerAppDelegate()
-- (void)_setupStatusItem;
-- (void)_updateStatusItemButtons;
-@end
+#import "StatusBarController.h"
 
 
 @implementation MyTunesControllerAppDelegate
 
-@synthesize window;
+@synthesize statusBarController = _statusBarController;
 
 + (void)initialize
 {
-	[[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
-															 [NSNumber numberWithUnsignedInteger:3], CONotificationCorner,    
-															 nil]];
+	if (self == [MyTunesControllerAppDelegate class]) 
+	{
+		[[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInteger:3], CONotificationCorner, nil]];
+	}
 }
 
-- (void)dealloc 
-{
-	[[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
-	[[NSUserDefaultsController sharedUserDefaultsController] removeObserver:self forKeyPath:@"values.NotificationCorner"];
-}
+
+#pragma mark Application Delegate
 
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender 
 {
 	return NSTerminateNow;
 }
 
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification 
 {
-	[[NSUserDefaultsController sharedUserDefaultsController] addObserver:self
-															  forKeyPath:@"values.NotificationCorner"
-																 options:NSKeyValueObservingOptionInitial
-																 context:nil];
+	[[NSUserDefaultsController sharedUserDefaultsController] addObserver:self forKeyPath:@"values.NotificationCorner" options:NSKeyValueObservingOptionInitial context:nil];
+	
 	[[iTunesController sharedInstance] setDelegate:self];
-	[self _setupStatusItem];
-	[self _updateStatusItemButtons];
+	
+	[self.statusBarController addStatusItems];
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context 
+
+- (void)applicationWillTerminate:(NSNotification *)notification
 {
-	if ([keyPath isEqualToString:@"values.NotificationCorner"]) {
-		positionCorner = [[[NSUserDefaults standardUserDefaults] objectForKey:CONotificationCorner] unsignedIntValue];
-		
-		if (notificationController)
-			[notificationController setPositionCorner:positionCorner];
-	} 
-	else 
-		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+	[[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
+	[[NSUserDefaultsController sharedUserDefaultsController] removeObserver:self forKeyPath:@"values.NotificationCorner"];
 }
 
-#pragma mark Delegates
+
+#pragma mark iTunesController Delegate
 
 - (void)iTunesTrackDidChange:(iTunesTrack *)newTrack
 {
-	[self _updateStatusItemButtons];
+	[self.statusBarController updatePlayButtonState];
 		
 	if (newTrack == nil) 
 		return;
 	
-	if ([[iTunesController sharedInstance] isPlaying] == NO) {
-		[notificationController disappear];
+	if ([[iTunesController sharedInstance] isPlaying] == NO) 
+	{
+		[notificationWindowController disappear];
 		return;
 	}
 	
 	// if I reused the window then text got blurred
-	if (notificationController) {
-		[notificationController setDelegate:nil];
-		[notificationController close];
-		notificationController = nil;
+	if (notificationWindowController) 
+	{
+		[notificationWindowController setDelegate:nil];
+		[notificationWindowController close];
+		notificationWindowController = nil;
 	}
-	notificationController = [[NotificationWindowController alloc] init];
-	[notificationController setDelegate:self];
-	[notificationController.window setAlphaValue:0.0];
-	[notificationController setTrack:newTrack];
-	[notificationController resize];
-	[notificationController setPositionCorner:positionCorner];
-	[notificationController showWindow:self];
+	
+	notificationWindowController = [[NotificationWindowController alloc] init];
+	[notificationWindowController setDelegate:self];
+	[notificationWindowController.window setAlphaValue:0.0];
+	[notificationWindowController setTrack:newTrack];
+	[notificationWindowController resize];
+	[notificationWindowController setPositionCorner:notificationCorner];
+	[notificationWindowController showWindow:self];
 }
+
+
+#pragma mark NotificationWindowController Delegate
 
 - (void)notificationCanBeRemoved 
 {
-	[notificationController close];
-	notificationController = nil;
+	[notificationWindowController close];
+	notificationWindowController = nil;
 }
+
+
+#pragma mark NSWindow Delegate
 
 - (void)windowWillClose:(NSNotification *)notification
 {
-	NSWindow *w = [notification object];
+	NSWindow *window = [notification object];
 	
-	if ([w isEqualTo:preferencesController.window]) {
-		preferencesController = nil;
-	}
+	if ([window isEqualTo:[preferencesWindowController window]]) 
+	{
+		preferencesWindowController = nil;
+	}	
+}
+
+
+#pragma mark KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context 
+{
+	if ([keyPath isEqualToString:@"values.NotificationCorner"]) 
+	{
+		notificationCorner = [[[NSUserDefaults standardUserDefaults] objectForKey:CONotificationCorner] unsignedIntValue];
 		
+		if (notificationWindowController)
+			[notificationWindowController setPositionCorner:notificationCorner];
+	} 
+	else 
+	{
+		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];	
+	}
 }
 
-#pragma mark Actions
 
-- (IBAction)playPrevious:(id)sender 
-{
-	[[iTunesController sharedInstance] playPrevious];
-}
+#pragma mark Managing Windows
 
-- (IBAction)playPause:(id)sender 
-{
-	[[iTunesController sharedInstance] playPause];
-}
-
-- (IBAction)playNext:(id)sender 
-{
-	[[iTunesController sharedInstance] playNext];
-}
-
-#pragma mark Private
-
-- (void)_aboutApp 
+- (void)showAboutPanel
 {
 	[NSApp orderFrontStandardAboutPanel:self];
 	[NSApp activateIgnoringOtherApps:YES];
 }
 
-- (void)_openPreferences 
+
+- (void)showPreferencesWindow
 {
-	if (preferencesController == nil) {
-		preferencesController = [[PreferencesController alloc] init];
-		preferencesController.window.delegate = self;
+	if (preferencesWindowController == nil) 
+	{
+		preferencesWindowController = [[PreferencesController alloc] init];
 	}
 	
-	[preferencesController showWindow:self];
+	[preferencesWindowController showWindow:self];
 	[NSApp activateIgnoringOtherApps:YES];
-}
-
-- (void)_quitApp 
-{
-	[NSApp terminate:self];
-}
-
-- (void)_setupStatusItem 
-{	
-	NSImage *statusIcon = [NSImage imageNamed:@"status_icon.png"];
-	controllerItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-	[controllerItem setImage:statusIcon];
-	[controllerItem setHighlightMode:YES];
-	
-	statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-	[statusItem setImage:[NSImage imageNamed:@"blank.png"]];
-	[statusItem setView:statusView];
-	
-	NSMenu *mainMenu = [[NSMenu alloc] init];
-	[mainMenu setAutoenablesItems:NO];
-	
-	NSMenuItem *theItem = [mainMenu addItemWithTitle:@"About"
-								  action:@selector(_aboutApp)
-						   keyEquivalent:@""];
-	[theItem setTarget:self];
-	
-	theItem = [mainMenu addItemWithTitle:@"Check for Updates..."
-								  action:@selector(checkForUpdates:)
-						   keyEquivalent:@""];
-	[theItem setTarget:sparkle];
-	
-	theItem = [mainMenu addItemWithTitle:@"Preferences..."
-								  action:@selector(_openPreferences)
-						   keyEquivalent:@""];
-	[theItem setTarget:self];
-	
-	[mainMenu addItem:[NSMenuItem separatorItem]];
-	
-	theItem = [mainMenu addItemWithTitle:@"Lyrics..."
-								  action:@selector(_openLyrics)
-						   keyEquivalent:@""];
-	[theItem setTarget:self];
-	
-	[mainMenu addItem:[NSMenuItem separatorItem]];
-	
-	
-	theItem = [mainMenu addItemWithTitle:@"Quit"
-								  action:@selector(_quitApp)
-						   keyEquivalent:@"Q"];
-	[theItem setTarget:self];
-	
-	[controllerItem setMenu:mainMenu];
-}
-
-- (void)_updateStatusItemButtons 
-{
-	if ([[iTunesController sharedInstance] isPlaying] == NO) {
-		[playButton setImage:[NSImage imageNamed:@"play"]];
-	}
-	else {
-		[playButton setImage:[NSImage imageNamed:@"pause"]];
-	}
 }
 
 @end
